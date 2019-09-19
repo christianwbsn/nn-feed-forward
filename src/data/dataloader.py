6ba0a685e33a2a4b5229be15ef4bb428def2b5f1
christianwbsn <christian.wibisono7@gmail.com>
2019-09-20 01:12:01 +0700

    implement mini batch

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,1 +1,18 @@
-# implement data splitting and mini batch here
+"""
+Module implementing data loading and splitting
+"""
+
+import numpy as np
+
+def mini_batch(inputs, targets, batch_size, shuffle=False):
+    assert inputs.shape[0] == targets.shape[0]
+    if shuffle:
+        indices = np.arange(inputs.shape[0])
+        np.random.shuffle(indices)
+    for start_idx in range(0, inputs.shape[0] - batch_size + 1, batch_size):
+        end_idx = min(start_idx + batch_size, inputs.shape[0])
+        if shuffle:
+            excerpt = indices[start_idx : end_idx]
+        else:
+            excerpt = slice(start_idx, end_idx)
+        yield inputs[excerpt], targets[excerpt]
