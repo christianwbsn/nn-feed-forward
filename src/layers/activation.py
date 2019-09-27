b718b3aad553ee8077ce379d5859a2ed71f16234
diwangs <27280516+diwangs@users.noreply.github.com>
2019-09-27 22:59:59 +0700

    Added comments

--- src/layers/activation.py
+++ src/layers/activation.py
@@ -21,10 +21,24 @@ class Sigmoid():
         return 1 / (1 + np.exp(-x))
 
     def __call__(self, x):
+        """
+        Calculate the elementwise sigmoid of a List
+
+        Takes a `List` of any size
+        Returns a `List` of the same size
+        """
+        # TODO: handle batch
         return [self.__elementwise_sigmoid__(x_i) for x_i in x]
 
     def calc_grad(self, x, succ_grad):
         """
-        with respect to each element in x
+        Calculate the derivative of sigmoid with respect to each element in x 
+        multiplied by all of its successive gradient
+
+        Takes:
+            x, a `List` of any size. The input
+            succ_grad, a `List` with the same size as `x`. The gradient of its successive layer
+        Returns a List with the same size as x. The resulting gradient
         """
+        # TODO: handle batch
         return [succ_grad[i] * self.__elementwise_sigmoid__(x[i]) * (1 - self.__elementwise_sigmoid__(x[i])) for i in range(len(x))]
--- src/layers/feedforward.py
+++ src/layers/feedforward.py
@@ -3,17 +3,25 @@ from numpy.random import rand
 class FeedForward():
     def __init__(self, input_dim, output_dim):
         self.weight = rand(output_dim, input_dim)
-        # print(self.weight.shape)
-        self.previous_input = None # For computing final gradient
-        # TODO: Initialize
 
     def __call__(self, x):
+        """
+        Apply weigths to input.
+
+        Takes a List of size input_dim
+        Returns a List of size output_dim
+        """
         # TODO: handle batch
         # TODO: Check shape of x
         return self.weight.dot(x)
 
     def calc_grad(self, succ_grad): # For computing error term
         """
-        With respect to its the input, not to its weight
+        Calculate the derivative of each the output with respect to its input (not to its weight) 
+        multiplied by all of its successive gradient
+        
+        Takes a List of size output_dim (the gradient of its successive layer)
+        Returns a List of size input_dim (the resulting gradient)
         """
+        # TODO: handle batch
         return [sum(succ_grad[j] * self.weight[j, i] for j in range(self.weight.shape[0])) for i in range(self.weight.shape[1])]
