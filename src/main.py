f5a8a790369907d75d9943e19036f5a21498b0fc
christianwbsn <christian.wibisono7@gmail.com>
2019-09-30 23:12:06 +0700

    update main to print loss

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from src.layers.model import Model
+from src.layers.loss import MeanSquaredError
 from src.data.dataloader import mini_batch
 
 
@@ -18,17 +19,22 @@ def train(inputs,labels):
                     help='momentum')
     args = parser.parse_args()
     model = Model([6, 6, 3, 1], hidden_layer=2, momentum=args.m, learning_rate=args.lr)
-    for _ in range(args.epoch):
+    criterion = MeanSquaredError()
+    for i in range(args.epoch):
+        running_loss = 0.0
         for batch in mini_batch(inputs, labels, args.b_size, shuffle=True):
             model(batch[0])
             model.back_prop(batch[1].reshape(-1,1))
+            running_loss = criterion(model(batch[0]), batch[1].reshape(-1,1))
+        loss = running_loss / args.b_size
+        print("Epoch {0}/{1} :{2}".format(i+1, args.epoch, loss))
     return model
+
 
 def predict(model, inputs, threshold=0.5):
     pred = model(inputs).flatten()
     pred = [1 if x>=threshold else 0 for x in pred]
     return pred
-
 
 if __name__ == '__main__':
     df = pd.read_csv('data/processed/dataset.csv')
